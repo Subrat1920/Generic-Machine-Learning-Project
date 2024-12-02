69e8a369b3fff582ab584738de323812f199043f
Subuking001 <3subratmishra1sep@gmail.com>
2024-12-03 00:14:47 +0530

    hyper-parameter tunning

--- src/componenets/model_trainer.py
+++ src/componenets/model_trainer.py
@@ -39,21 +39,84 @@ class ModelTrainer:
             )
             models = {
                 'Linear Regression' : LinearRegression(),
-                'Ridge Regression' : Ridge(),
-                'Lasso Regression' : Lasso(),
-                'Elastic Net Regresion' : ElasticNet(),
+                # 'Ridge Regression' : Ridge(),
+                # 'Lasso Regression' : Lasso(),
+                # 'Elastic Net Regression' : ElasticNet(),
                 'K Nearest Neighbors' : KNeighborsRegressor(),
-                'Support Vector Regressor' : SVR(),
-                'Decsion Tree Regressor' : DecisionTreeRegressor(),
+                # 'Support Vector Regressor' : SVR(),
+                'Decision Tree Regressor': DecisionTreeRegressor(),
                 'Random Forest Regressor' : RandomForestRegressor(),
                 'Gradient Boost Regressor' : GradientBoostingRegressor(),
                 'Cat Boost Regressor' : CatBoostRegressor(verbose=False),
-                'Extra Gradient Boost Regressor' : XGBRegressor()
+                'XGBoost Regressor' : XGBRegressor()
+
             }
+
+            params = {
+                'Linear Regression': {
+                    # 'fit_intercept': [True, False],
+                    # 'normalize': [True, False],
+                    # 'copy_X': [True, False]
+                },
+                'Ridge Regression': {
+                    'alpha': [0.1, 1.0, 10.0],
+                    'solver': ['lsqr', 'sparse_cg', 'sag', 'saga'] ## also we can add ['auto', 'svd', 'cholesky']
+                },
+                'Lasso Regression': {
+                    'alpha': [0.1, 1.0, 10.0],
+                    'max_iter': [1000, 5000, 10000],
+                    'selection': ['cyclic', 'random']
+                },
+                'Elastic Net Regression': {
+                    'alpha': [0.1, 1.0, 10.0],
+                    'l1_ratio': [0.1, 0.5, 0.9],
+                    'max_iter': [1000, 5000, 10000]
+                },
+                'K Nearest Neighbors': {
+                    'n_neighbors': [3, 5, 7, 10],
+                    'weights': ['uniform', 'distance'],
+                    'algorithm': ['ball_tree', 'kd_tree']
+                },
+                'Support Vector Regressor': {
+                    'kernel': ['linear', 'poly', 'rbf', 'sigmoid'],
+                    'C': [0.1, 1.0, 10.0],
+                    'epsilon': [0.1, 0.2, 0.5]
+                },
+                'Decision Tree Regressor': {
+                    'criterion': ['squared_error', 'friedman_mse', 'poisson'],
+                    'splitter': ['best', 'random'],
+                    'max_depth': [None, 10, 20, 30],
+                    'min_samples_split': [2, 5, 10]
+                },
+                'Random Forest Regressor': {
+                    'n_estimators': [100, 200, 300],
+                    'criterion': ['squared_error', 'absolute_error', 'poisson'],
+                    'max_depth': [None, 10, 20, 30],
+                    'min_samples_split': [2, 5, 10]
+                },
+                'Gradient Boost Regressor': {
+                    'loss': ['squared_error', 'absolute_error'],
+                    'learning_rate': [0.01, 0.1, 0.2],
+                    'n_estimators': [100, 200, 300],
+                    'subsample': [0.5, 0.7, 1.0]
+                },
+                'Cat Boost Regressor': {
+                    'iterations': [100, 500, 1000],
+                    'learning_rate': [0.01, 0.1, 0.2],
+                    'depth': [4, 6, 10]
+                },
+                'XGBoost Regressor': {
+                    'n_estimators': [100, 200, 300],
+                    'learning_rate': [0.01, 0.1, 0.2],
+                    'max_depth': [3, 6, 9],
+                    'subsample': [0.5, 0.7, 1.0]
+                }
+}
+
 
             ## below code is getting changed for some error if not required, undo it 
             ## changing x=x_train, y=y_train to x_train=x_train, y_train=y_train
-            model_report : dict = evaluate_models(x_train=x_train, y_train=y_train, x_test=x_test, y_test=y_test, models = models)
+            model_report : dict = evaluate_models(x_train=x_train, y_train=y_train, x_test=x_test, y_test=y_test, models = models, param=params)
             
             ## to get the best model score from dict
             best_model_score = max(sorted(model_report.values()))
--- src/utils.py
+++ src/utils.py
@@ -1,10 +1,6 @@
-# "utils.py" typically refers to a file containing 
-# a collection of small, reusable functions and 
-# helper classes that perform common tasks across 
-# different parts of the application, essentially 
-# acting as a "utility belt" for frequently used 
-# operations, thus reducing code duplication and 
-# improving code organization. 
+# "utils.py" typically refers to a file containing a collection of small, reusable functions and 
+# helper classes that perform common tasks across different parts of the application, essentially acting as a "utility belt" for frequently used 
+# operations, thus reducing code duplication and improving code organization. 
 
 import os
 import sys
@@ -15,6 +11,7 @@ from src.exception import CustomException
 import dill
 
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 
 
 ## save_object fuction that is been used in 
@@ -32,12 +29,24 @@ def save_objects(file_path, obj):
 
 ## this function is used in model_training.py
 ## for evalution of the training data
-def evaluate_models(x_train,y_train,x_test, y_test,models):
+def evaluate_models(x_train,y_train,x_test, y_test,models, param):
     try:
         report = {}
 
         for i in range(len(list(models))):
             model = list(models.values())[i]
+            para = param[list(models.keys())[i]]
+            
+            ## Grid Search CV
+            gs = GridSearchCV(estimator=model, param_grid=para, cv=5)
+
+            ## fixing with grid search CV
+            gs.fit(x_train, y_train)
+            
+            ## setting the best params from hyper-parameter tunning to mode
+            model.set_params(**gs.best_params_)
+
+            ## training the model
             model.fit(x_train, y_train)
 
             ## making prediction
@@ -50,6 +59,40 @@ def evaluate_models(x_train,y_train,x_test, y_test,models):
             report[list(models.keys())[i]] = test_model_score
 
             return report
-    
-    except:
-        pass
+
+# def evaluate_models(x_train, y_train, x_test, y_test, models, param):
+#     try:
+#         report = {}
+
+#         for model_name, model in models.items():
+#             para = param.get(model_name, None)
+            
+#             # Ensure parameters are provided for the model
+#             if para is None:
+#                 raise ValueError(f"Parameter grid for {model_name} is not provided.")
+
+#             # Perform Grid Search CV
+#             gs = GridSearchCV(estimator=model, param_grid=para, cv=5, scoring='r2')
+#             gs.fit(x_train, y_train)
+
+#             # Use the best model found in Grid Search
+#             best_model = gs.best_estimator_
+
+#             # Fit the best model to the training data
+#             best_model.fit(x_train, y_train)
+
+#             # Make predictions
+#             y_train_pred = best_model.predict(x_train)
+#             y_test_pred = best_model.predict(x_test)
+
+#             # Calculate R2 scores
+#             train_model_score = r2_score(y_train, y_train_pred)
+#             test_model_score = r2_score(y_test, y_test_pred)
+
+#             # Store the test score in the report
+#             report[model_name] = test_model_score
+
+#         return report
+
+    except Exception as e:
+        raise CustomException(e, sys)
